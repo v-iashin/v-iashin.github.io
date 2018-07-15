2fd3c4e876dddcbaf955265ac99c2e123a02f76d
Vladimir Iashin <vdyashin@gmail.com>
2018-07-15 21:46:33 +0300

    v1 release

---
+++ js/status_checker.js
@@ -0,0 +1,14 @@
+//// url to server with flask running 
+//var SERVER_URL = 'https://vdyashin.ml:5000/status_check';
+//
+//$.ajax({
+//  //your server url
+//  url: SERVER_URL,
+//  type: 'GET',
+//  success: function(){
+//    document.getElementById('status').innerHTML = "<span id='status_ok'> &#9679 </span>";
+//  },
+//  error: function(){
+//    document.getElementById('status').innerHTML = "<span id='status_down'> &#9679 </span>";
+//  }
+//});
--- js/upload_handler.js
+++ js/upload_handler.js
@@ -1,62 +1,80 @@
 // url to server with flask running 
-var SERVER_URL = 'https://vdyashin.ml:5000/'
+var SERVER_URL = 'https://vdyashin.ml:5000/';
+var MAX_SIDE_LEN = 1280;
 
 // adapted from https://codepen.io/nakome/pen/vmKwQg
 // vars
-let result = document.querySelector('.result'),
-img_result = document.querySelector('.img-result'),
-//img_w = document.querySelector('.img-w'),
-//img_h = document.querySelector('.img-h'),
-options = document.querySelector('.options'),
-save = document.querySelector('.save'),
+preview = document.querySelector('.preview'),
+cropped_candidate = document.querySelector('.cropped_candidate'),
+crop = document.querySelector('.crop'),
 cropped = document.querySelector('.cropped'),
 detect = document.querySelector('.detect'),
 upload = document.querySelector('#file-input'),
 cropper = '';
 
-// on change show image with crop options
-upload.addEventListener('change', (e) => {
-  if (e.target.files.length) {
+// on change show image
+upload.addEventListener('change', function() {
+  if (event.target.files.length) {
     // start file reader
     const reader = new FileReader();
-    reader.onload = (e) => {
-      if(e.target.result){
+    reader.onload = function() {
+      if(event.target.result) {
         // create new image
         let img = document.createElement('img');
         img.id = 'image';
-        img.src = e.target.result
+        img.src = event.target.result;
         // clean result before
-        result.innerHTML = '';
+        preview.innerHTML = '';
         // append new image
-        result.appendChild(img);
-        // show save btn and options
-        save.classList.remove('hide');
+        preview.appendChild(img);
+        // show crop btn
+        crop.classList.remove('hide');
         // init cropper
         cropper = new Cropper(img);
       }
     };
-    reader.readAsDataURL(e.target.files[0]);
+    reader.readAsDataURL(event.target.files[0]);
   }
 });
 
-// save on click
-save.addEventListener('click', (e)=>{
-  e.preventDefault();
+// crop on click
+crop.addEventListener('click', function() {
+  event.preventDefault();
   // get result to data uri
-  let imgSrc = cropper.getCroppedCanvas().toDataURL("image/jpeg");
+  var cropped_canvas = cropper.getCroppedCanvas()
+  let imgSrc = cropped_canvas.toDataURL("image/jpeg");
+  cropped.src = imgSrc;
+  // client side reduce the photo size https://embed.plnkr.co/oyaVFn/
+  var image = new Image();
+  image.onload=function(){
+    var canvas=document.createElement("canvas");
+    var context=canvas.getContext("2d");
+    [canvas.width, canvas.height] = reduceSize(cropped_canvas.width, cropped_canvas.height, MAX_SIDE_LEN);
+    context.drawImage(image,
+      0,
+      0,
+      image.width,
+      image.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    cropped.src = canvas.toDataURL();
+  }
+  image.src = cropped.src;
   // remove hide class of img
   cropped.classList.remove('hide');
-	img_result.classList.remove('hide');
-	// show image cropped
-  cropped.src = imgSrc;
+  cropped_candidate.classList.remove('hide');
   detect.classList.remove('hide');
-//  detect.download = 'imagename.png';
-//  detect.setAttribute('href', imgSrc);
 });
 
-// save on click
-detect.addEventListener('click', (e)=>{
-  e.preventDefault();
+// detect on click
+detect.addEventListener('click', function() {
+  // progress status
+  document.getElementById('progress').innerHTML = 'Processing...';
+  
+  event.preventDefault();
   // get result to data uri
   var blob = dataURItoBlob(cropped.src);
   var form_data = new FormData();
@@ -65,6 +83,7 @@ detect.addEventListener('click', (e)=>{
     type: 'POST',
     url: SERVER_URL,
     data: form_data,
+    timeout: 1000 * 10, // ms
     contentType: false,
     processData: false,
     dataType: 'json',
@@ -73,34 +92,49 @@ detect.addEventListener('click', (e)=>{
     console.log(textStatus);
     console.log(jqXHR);
     console.log('Success!');
-//      $("#resultFilename").text("Filename: " + data['name']);
-//      $("#resultFilesize").text("File size: " + data['size']);
-//      $('#resultImg').append('<img src="' + data['image'] + '" />');
     cropped.src = data['image'];
+    // progress status
+    document.getElementById('progress').innerHTML = 'Done';
   }).fail(function(data){
-//    alert('error!');
     console.log("ERRORRRR: ");
+    alert('It seems that the detector is not working right now but you tried to upload an image. If you are still interested please contact me.');
   });
 });
 
 
 // https://stackoverflow.com/a/5100158/3671347
 function dataURItoBlob(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+    byteString = atob(dataURI.split(',')[1]);
+  else
+    byteString = unescape(dataURI.split(',')[1]);
 
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
 
-    return new Blob([ia], {type:mimeString});
+  return new Blob([ia], {type:mimeString});
+}
+
+function reduceSize(width, height, max_side_len) {
+  if (Math.max(width, height) <= max_side_len) {
+    return [width, height];
+  }
+  else if (width >= height) {
+    height *= max_side_len / width;
+    width = max_side_len;
+    return [width, height];
+  }
+  else if (width < height) {
+    width *= max_side_len / height;
+    height = max_side_len;
+    return [width, height];
+  }
 }
