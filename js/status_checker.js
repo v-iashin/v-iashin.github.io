722e97730ca739428fc80822428556ea081e751c
Vladimir Iashin <vdyashin@gmail.com>
2018-12-10 08:28:02 +0200

    last name update, new username, links update, currently face detector is offline

--- js/status_checker.js
+++ js/status_checker.js
@@ -1,5 +1,6 @@
 // url to server with flask running 
-var STATUS_CHECK_URL = 'https://vdyashin.ml:5000/status_check';
+var STATUS_CHECK_URL = 'https://v-iashin.ml:5000/status_check';
+// try certified IP (but note the possible ephemeralizm)
 
 // by default it is down
 document.getElementById('status').innerHTML = "<span id='status_down' alt='down'> &#9679 </span>";
