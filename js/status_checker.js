55710afe9b4c680f50e79c5adeae204e3a2d3209
v-iashin <vdyashin@gmail.com>
2019-09-15 10:39:40 +0300

    switched the domain to v-iashin.ml

--- js/status_checker.js
+++ js/status_checker.js
@@ -1,5 +1,5 @@
 // url to server with flask running 
-var STATUS_CHECK_URL = 'https://v-iashin.tk:5000/status_check';
+var STATUS_CHECK_URL = 'https://v-iashin.ml:5000/status_check';
 
 // try certified IP (but note the possible ephemeralizm)
 
